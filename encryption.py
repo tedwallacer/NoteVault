819bced5c3e9ddd45c0dda3539503697a8b497a1
tedwallacer <163332348+tedwallacer@users.noreply.github.com>
2024-05-10 00:59:19 +0200

    optimization

--- encryption.py
+++ encryption.py
@@ -1,6 +1,5 @@
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 import os
@@ -12,35 +11,39 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY').encode()
 SALT = os.getenv('SALT').encode()
 
-def generate_derived_key():
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=SALT,
-        iterations=390000,
-        backend=default_backend()
-    )
-    return base64.urlsafe_b64encode(kdf.derive(SECRET_KEY))
+class NoteVault:
+    _fernet_instance = None
 
-def get_fernet_instance():
-    key = generate_derived_key()
-    fernet = Fernet(key)
-    return fernet
+    @classmethod
+    def get_fernet_instance(cls):
+        if cls._fernet_instance is None:
+            kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=SALT,
+                iterations=390000,
+                backend=default_backend()
+            )
+            key = base64.urlsafe_b64encode(kdf.derive(SECRET_KEY))
+            cls._fernet_instance = Fernet(key)
+        return cls._fernet_instance
 
-def encrypt_message(message):
-    fernet = get_fernet_instance()
-    encrypted_message = fernet.encrypt(message.encode())
-    return encrypted_message.decode()
+    @staticmethod
+    def encrypt_message(message):
+        fernet = NoteVault.get_fernet_instance()
+        encrypted_message = fernet.encrypt(message.encode())
+        return encrypted_message.decode()
 
-def decrypt_message(encrypted_message):
-    fernet = get_fernet_instance()
-    decrypted_message = fernet.decrypt(encrypted_message.encode())
-    return decrypted_message.decode()
+    @staticmethod
+    def decrypt_message(encrypted_message):
+        fernet = NoteVault.get_fernet_instance()
+        decrypted_message = fernet.decrypt(encrypted_message.encode())
+        return decrypted_message.decode()
 
 if __name__ == "__main__":
     test_message = "This is a test"
-    encrypted = encrypt_message(test_message)
+    encrypted = NoteVault.encrypt_message(test_message)
     print(f"Encrypted: {encrypted}")
     
-    decrypted = decrypt_message(encrypted)
+    decrypted = NoteVault.decrypt_message(encrypted)
     print(f"Decrypted: {decrypted}")
